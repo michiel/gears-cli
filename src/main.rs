0c60b741868439df7d93fbf05c5360f4d63bf312
Michiel Kalkman <michiel@nosuchtype.com>
2017-07-20 08:41:42 +1000

    Update to version v0.1.6

--- src/main.rs
+++ src/main.rs
@@ -39,7 +39,7 @@ fn main() {
     let _ = env_logger::init();
 
     let matches = App::new("gears-cli")
-        .version("0.1.4")
+        .version("0.1.6")
         .author("Michiel Kalkman <michiel@nosuchtype.com")
         .about("CLI tool for working with gears-project models")
         .arg(Arg::with_name("config")
