6a843289043b1713ee830e8701f30eb20eb28e97
Michiel Kalkman <michiel@nosuchtype.com>
2018-09-01 21:04:33 +1000

    Add web interface
    Signed-off-by: Michiel Kalkman <michiel@nosuchtype.com>

---
+++ src/model_executor.rs
@@ -0,0 +1,189 @@
+use gears::structure::model::ModelDocument;
+use gears::structure::common::ModelLoadError;
+use gears;
+
+use actix::prelude::*;
+use futures::Future;
+
+fn load_model(path: &str) -> Result<ModelDocument, InputError> {
+    match gears::util::fs::model_from_fs(path) {
+        Ok(model) => Ok(model),
+        Err(_) => Err(InputError::IOError)
+    }
+}
+
+#[derive(Debug)]
+pub enum InputError {
+    IOError,
+    BadFormat(String),
+}
+
+pub trait ModelStore {
+    fn list(&self) -> Result<Vec<ModelDocument>, InputError>;
+    fn get(&self, id: &str) -> Result<ModelDocument, InputError>;
+    fn new(&self) -> Result<ModelDocument, InputError>;
+    fn create(&self, json: &str) -> Result<ModelDocument, InputError>;
+    fn update(&self, json: &str) -> Result<ModelDocument, InputError>;
+    fn delete(&self, json: &str) -> Result<(), InputError>;
+}
+
+#[derive(Clone)]
+pub struct FileSystemModelStore {
+    root: String,
+}
+
+impl FileSystemModelStore {
+    pub fn new(path: &str) -> Result<Self, ModelLoadError> {
+        match load_model(&path) {
+            Ok(_) => {
+                Ok(FileSystemModelStore {
+                    root: path.to_owned()
+                })
+            },
+            Err(err) => Err(ModelLoadError::BadStructure("Unable to init".to_owned()))
+        }
+    }
+}
+
+impl ModelStore for FileSystemModelStore {
+    fn list(&self) -> Result<Vec<ModelDocument>, InputError> {
+        match load_model(&self.root) {
+            Ok(res) => Ok(vec![res]),
+            Err(_) => Err(InputError::IOError)
+        }
+    }
+
+    fn get(&self, _id: &str) -> Result<ModelDocument, InputError> {
+        load_model(&self.root)
+    }
+
+    fn new(&self) -> Result<ModelDocument, InputError> {
+        info!("init: in directory {}", self.root);
+        match gears::util::fs::init_new_model_dir(&self.root) {
+            Ok(_) => load_model(&self.root),
+            Err(err) => {
+                let msg = format!("{:?}", err);
+                Err(InputError::BadFormat(msg))
+            },
+        }
+    }
+
+    fn create(&self, json: &str) -> Result<ModelDocument, InputError> {
+        info!("create: in directory {}", self.root);
+        match gears::structure::model::ModelDocument::from_json(&json) {
+            Ok(model) => {
+                match gears::util::fs::model_to_fs(&model, &self.root) {
+                    Ok(_) => load_model(&self.root),
+                    Err(_) => Err(InputError::IOError),
+                }
+            }
+            Err(err) => Err(InputError::BadFormat(format!("{:?}", err)))
+        }
+    }
+
+    fn update(&self, json: &str) -> Result<ModelDocument, InputError> {
+        info!("update: in directory {}", self.root);
+        match gears::structure::model::ModelDocument::from_json(&json) {
+            Ok(model) => {
+                match gears::util::fs::model_to_fs(&model, &self.root) {
+                    Ok(_) => load_model(&self.root),
+                    Err(_) => Err(InputError::IOError),
+                }
+            }
+            Err(err) => Err(InputError::BadFormat(format!("{:?}", err)))
+        }
+    }
+
+    fn delete(&self, json: &str) -> Result<(), InputError> {
+        unimplemented!()
+    }
+}
+
+impl Actor for FileSystemModelStore {
+    type Context = SyncContext<Self>;
+
+    fn started(&mut self, ctx: &mut SyncContext<Self>) {
+        println!("Actor is alive");
+    }
+
+    fn stopped(&mut self, ctx: &mut SyncContext<Self>) {
+        println!("Actor is stopped");
+    }
+}
+
+struct ModelStoreList;
+
+impl Message for ModelStoreList {
+    type Result = Result<Vec<ModelDocument>, InputError>;
+}
+
+impl Handler<ModelStoreList> for FileSystemModelStore {
+    type Result = Result<Vec<ModelDocument>, InputError>;
+
+    fn handle(&mut self, msg: ModelStoreList, ctx: &mut SyncContext<Self>) -> Self::Result {
+        self.list()
+    }
+}
+
+struct ModelStoreGet<'a> {
+    id: &'a str
+}
+
+impl<'a> Message for ModelStoreGet<'a> {
+    type Result = Result<ModelDocument, InputError>;
+}
+
+impl<'a> Handler<ModelStoreGet<'a>> for FileSystemModelStore {
+    type Result = Result<ModelDocument, InputError>;
+
+    fn handle(&mut self, msg: ModelStoreGet, ctx: &mut SyncContext<Self>) -> Self::Result {
+        self.get(&msg.id)
+    }
+}
+
+struct ModelStoreNew;
+
+impl Message for ModelStoreNew {
+    type Result = Result<ModelDocument, InputError>;
+}
+
+impl Handler<ModelStoreNew> for FileSystemModelStore {
+    type Result = Result<ModelDocument, InputError>;
+
+    fn handle(&mut self, msg: ModelStoreNew, ctx: &mut SyncContext<Self>) -> Self::Result {
+        self.new()
+    }
+}
+
+struct ModelStoreCreate<'a> {
+    json: &'a str
+}
+
+impl<'a> Message for ModelStoreCreate<'a> {
+    type Result = Result<ModelDocument, InputError>;
+}
+
+impl<'a> Handler<ModelStoreCreate<'a>> for FileSystemModelStore {
+    type Result = Result<ModelDocument, InputError>;
+
+    fn handle(&mut self, msg: ModelStoreCreate, ctx: &mut SyncContext<Self>) -> Self::Result {
+        self.create(msg.json)
+    }
+}
+
+struct ModelStoreDelete<'a> {
+    id: &'a str
+}
+
+impl<'a> Message for ModelStoreDelete<'a> {
+    type Result = Result<(), InputError>;
+}
+
+impl<'a> Handler<ModelStoreDelete<'a>> for FileSystemModelStore {
+    type Result = Result<(), InputError>;
+
+    fn handle(&mut self, msg: ModelStoreDelete, ctx: &mut SyncContext<Self>) -> Self::Result {
+        self.delete(msg.id)
+    }
+}
+
---
+++ src/server.rs
@@ -0,0 +1,337 @@
+
+//! Actix web juniper example
+//!
+//! A simple example integrating juniper in actix-web
+
+use gears::structure::model::ModelDocument;
+
+use actix::prelude::*;
+use actix_web::{
+    http, middleware, server, App, AsyncResponder, Error, FutureResponse, HttpRequest,
+    HttpResponse, HttpMessage, Json, State, http::Method, http::StatusCode, pred,
+};
+use futures::future::Future;
+use juniper::http::graphiql::graphiql_source;
+use juniper::http::GraphQLRequest;
+use std::sync::Arc;
+
+use gears;
+use model_schema::create_schema;
+use model_schema::Schema;
+use model_executor::FileSystemModelStore;
+use model_executor::ModelStore;
+use bytes::Bytes;
+use std::str;
+use serde_json;
+
+struct AppState {
+    executor: Addr<GraphQLExecutor>,
+    modelstore: FileSystemModelStore,
+}
+
+#[derive(Serialize, Deserialize)]
+pub struct GraphQLData(GraphQLRequest);
+
+impl Message for GraphQLData {
+    type Result = Result<String, Error>;
+}
+
+pub struct GraphQLExecutor {
+    schema: Arc<Schema>,
+}
+
+impl GraphQLExecutor {
+    fn new(schema: Arc<Schema>) -> GraphQLExecutor {
+	GraphQLExecutor { schema: schema }
+    }
+}
+
+impl Actor for GraphQLExecutor {
+    type Context = SyncContext<Self>;
+}
+
+impl Handler<GraphQLData> for GraphQLExecutor {
+    type Result = Result<String, Error>;
+
+    fn handle(&mut self, msg: GraphQLData, _: &mut Self::Context) -> Self::Result {
+	let res = msg.0.execute(&self.schema, &());
+	let res_text = serde_json::to_string(&res)?;
+	Ok(res_text)
+    }
+}
+
+fn graphiql(_req: &HttpRequest<AppState>) -> Result<HttpResponse, Error> {
+    let html = graphiql_source("/graphql/api");
+    Ok(HttpResponse::Ok()
+       .content_type("text/html; charset=utf-8")
+       .body(html))
+}
+
+fn graphql(
+    (st, data): (State<AppState>, Json<GraphQLData>),
+    ) -> FutureResponse<HttpResponse> {
+    st.executor
+	.send(data.0)
+	.from_err()
+	.and_then(|res| match res {
+	    Ok(obj) => Ok(HttpResponse::Ok()
+			   .content_type("application/json")
+			   .body(obj)),
+	    Err(_) => Ok(HttpResponse::InternalServerError().into()),
+	})
+    .responder()
+}
+
+static CONTENT_TYPE_JSON: &'static str = "application/json; charset=utf-8";
+
+pub fn serve(path: &str) {
+    ::std::env::set_var("RUST_LOG", "actix_web=info");
+    let sys = actix::System::new("model-graphql");
+
+    let schema = Arc::new(create_schema());
+    let addr_graphql = SyncArbiter::start(3, move || GraphQLExecutor::new(schema.clone()));
+
+    let modelstore = match FileSystemModelStore::new(&path) {
+	Ok(res) => res,
+	Err(_) => {
+	    error!("Unable to initialize model. Is this a model direcory?");
+	    return ()
+	}
+    };
+
+    server::new(move || {
+	let graphql_app = App::with_state(AppState{
+	    executor: addr_graphql.clone(),
+	    modelstore: modelstore.clone(),
+	})
+	.prefix("graphql")
+	    .middleware(middleware::Logger::default())
+	    .resource("/api", |r| r.method(http::Method::POST).with(graphql))
+	    .resource("/graphiql", |r| r.method(http::Method::GET).h(graphiql))
+	    .resource("/", |r| r.f(graphql_index))
+	    .resource("", |r| r.f(graphql_index))
+	    .default_resource(|r| {
+		r.method(Method::GET).f(p404);
+		r.route().filter(pred::Not(pred::Get())).f(
+		    |_| HttpResponse::MethodNotAllowed());
+	    });
+
+	let jsonapi_app = App::with_state(AppState{
+	    executor: addr_graphql.clone(),
+	    modelstore: modelstore.clone(),
+	})
+	.prefix("jsonapi")
+	    .middleware(middleware::Logger::default())
+	    .scope("/model", |model_scope| {
+		model_scope
+		    .resource("", |r| {
+			r.method(Method::GET).f(get_models);
+			r.method(Method::POST).with(create_model);
+		    })
+		.resource("/{model_id}", |r| {
+		    r.method(Method::GET).f(get_model);
+		    r.method(Method::PUT).f(update_model);
+		    // r.method(Method::DELETE).f(get_model)
+		})
+		/*
+		.nested("/{model_id}/pages", |page_scope| {
+		    page_scope
+			.resource("", |r| {
+			    r.method(Method::GET).f(get_pages);
+			    // r.method(Method::POST).f(get_page);
+			})
+		    .resource("/{page_id}", |r| {
+			r.method(Method::GET).f(get_page);
+			// r.method(Method::PUT).with(get_page);
+			// r.method(Method::DELETE).with(get_page)
+		    })
+		})
+		.nested("/{model_id}/xflows", |xflow_scope| {
+		    xflow_scope
+			.resource("", |r| {
+			    r.method(Method::GET).f(get_xflows);
+			    // r.method(Method::POST).f(get_page);
+			})
+		    .resource("/{xflow_id}", |r| {
+			r.method(Method::GET).f(get_xflow);
+			// r.method(Method::PUT).with(get_page);
+			// r.method(Method::DELETE).with(get_page)
+		    })
+		})
+		*/
+	    })
+
+	.resource("/", |r| r.f(jsonapi_index))
+	    .resource("", |r| r.f(jsonapi_index)
+		     )
+
+	    .default_resource(|r| {
+		r.method(Method::GET).f(p404);
+		r.route().filter(pred::Not(pred::Get())).f(
+		    |req| HttpResponse::MethodNotAllowed());
+	    });
+
+	vec![
+	    graphql_app,
+	    jsonapi_app
+	]
+
+    }).bind("127.0.0.1:8080")
+    .unwrap()
+	.start();
+
+    println!("Started http server: 127.0.0.1:8080");
+    let _ = sys.run();
+}
+
+
+fn graphql_index(_req: &HttpRequest<AppState>) -> HttpResponse {
+    HttpResponse::Found()
+	.header("LOCATION", "graphiql")
+	.finish()
+}
+
+fn jsonapi_index(req: &HttpRequest<AppState>) -> HttpResponse {
+    HttpResponse::Found()
+	.header("LOCATION", format!("api/model/1"))
+	.finish()
+}
+
+fn p404(req:&HttpRequest<AppState>) -> HttpResponse {
+    HttpResponse::build(StatusCode::NOT_FOUND).finish()
+}
+//
+// Models
+
+fn get_models(req:&HttpRequest<AppState>) -> HttpResponse {
+    match &req.state().modelstore.get(&"") {
+	Ok(res) => {
+	    HttpResponse::build(StatusCode::OK)
+		.content_type(CONTENT_TYPE_JSON)
+		.body(
+		    format!("[{}]", res.to_json_compact())
+		    )
+	},
+	Err(_) => {
+	    HttpResponse::build(StatusCode::NOT_FOUND).finish()
+	}
+    }
+}
+
+fn get_model(req:&HttpRequest<AppState>) -> HttpResponse {
+    let model_id = &req.match_info()["model_id"];
+    match &req.state().modelstore.get(&model_id) {
+	Ok(res) => {
+	    HttpResponse::build(StatusCode::OK)
+		.content_type(CONTENT_TYPE_JSON)
+		.body(
+		    format!("{}", res.to_json_compact())
+		    )
+	},
+	Err(_) => {
+	    HttpResponse::build(StatusCode::NOT_FOUND).finish()
+	}
+    }
+}
+
+fn create_model(model: Json<ModelDocument>) -> HttpResponse {
+    HttpResponse::build(StatusCode::OK)
+	.content_type(CONTENT_TYPE_JSON)
+	.body(format!("{}", model.to_json_compact()))
+}
+
+fn update_model(req: &HttpRequest<AppState>) -> FutureResponse<HttpResponse> {
+    // format!("{}", model.to_json());
+
+    let req = req.clone();
+    req
+	.body()
+	.from_err()
+	.and_then(move |bytes: Bytes| {
+	    match str::from_utf8(&bytes) {
+		Ok(body) => {
+		    // println!("==== BODY ==== {:?}", body);
+		    match &req.state().modelstore.update(&body) {
+			Ok(res) => {
+			    Ok(HttpResponse::build(StatusCode::OK)
+			       .content_type(CONTENT_TYPE_JSON)
+			       .body( format!("{}", res.to_json_compact()))
+			      )
+			},
+			Err(err) => {
+			    Ok(HttpResponse::build(StatusCode::BAD_REQUEST)
+			       .content_type(CONTENT_TYPE_JSON)
+			       .body( format!("{:?}", err))
+			      )
+			}
+		    }
+		},
+		Err(_) => {
+		    Ok(HttpResponse::build(StatusCode::BAD_REQUEST)
+		       .content_type(CONTENT_TYPE_JSON)
+		       .body(format!("Invalid JSON"))
+		      )
+		}
+	    }
+	})
+    .responder()
+}
+
+/*
+//
+// Pages
+
+fn get_pages(req:&HttpRequest<AppState>) -> impl Responder {
+    let model_id = &req.match_info()["model_id"];
+    let model_uuid =  Uuid::parse_str(model_id);
+
+    format!("{}",
+	    serde_json::to_string_pretty(
+		&req.state().model.body.pages
+		).unwrap()
+	   )
+}
+
+fn get_page(req:&HttpRequest<AppState>) -> impl Responder {
+    let model_id = &req.match_info()["model_id"];
+    let model_uuid =  Uuid::parse_str(model_id);
+
+    let page_id = &req.match_info()["page_id"];
+    let page_uuid =  Uuid::parse_str(page_id).unwrap();
+
+    format!("{}",
+	    serde_json::to_string_pretty(
+		&req.state().model.get_page(&page_uuid).unwrap()
+		).unwrap()
+	   )
+}
+
+//
+// XFlows
+
+fn get_xflows(req:&HttpRequest<AppState>) -> impl Responder {
+    let model_id = &req.match_info()["model_id"];
+    let model_uuid =  Uuid::parse_str(model_id);
+
+    format!("{}",
+	    serde_json::to_string_pretty(
+		&req.state().model.body.xflows
+		).unwrap()
+	   )
+}
+
+fn get_xflow(req:&HttpRequest<AppState>) -> impl Responder {
+    let model_id = &req.match_info()["model_id"];
+    let model_uuid =  Uuid::parse_str(model_id);
+
+    let xflow_id = &req.match_info()["xflow_id"];
+    let xflow_uuid =  Uuid::parse_str(xflow_id).unwrap();
+
+    format!("{}",
+	    serde_json::to_string_pretty(
+		&req.state().model.get_xflow(&xflow_uuid).unwrap()
+		).unwrap()
+	   )
+}
+
+*/
