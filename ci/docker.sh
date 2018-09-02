755554f1786d3531dbd4f910e6b0ee5589b461f6
Michiel Kalkman <michiel@nosuchtype.com>
2018-09-03 08:23:19 +1000

    Add docker script after_deploy
    Signed-off-by: Michiel Kalkman <michiel@nosuchtype.com>

---
+++ ci/docker.sh
@@ -0,0 +1,25 @@
+set -ex
+
+main() {
+    echo $DOCKER_PASS | docker login -u $DOCKER_USER --password-stdin
+    # - docker login -e $DOCKER_EMAIL -u $DOCKER_USER -p $DOCKER_PASS
+    export REPO=gearsproject/gears
+    export TAG=$TRAVIS_BRANCH
+    echo $REPO
+    echo $TAG
+    echo $TRAVIS_COMMIT
+    # - docker build -f Dockerfile -t $REPO:$COMMIT .
+    docker build . -f Dockerfile -t $REPO:$TRAVIS_COMMIT
+    docker tag $REPO:$TRAVIS_COMMIT $REPO:$TAG
+    docker tag $REPO:$TRAVIS_COMMIT $REPO:travis-$TRAVIS_BUILD_NUMBER
+    docker push $REPO
+}
+
+echo "Checking for TARGET - $TARGET"
+# if [ "$TARGET" == "x86_64-unknown-linux-musl" ] && [ -z $TRAVIS_TAG ]; then
+if [ "$TARGET" == "x86_64-unknown-linux-musl" ]; then
+    echo "TARGET matched for docker build"
+    main
+else
+  echo "TARGET not matched for docker build"
+fi
